fb7d21cc3fe9a9c5f2ea423f82c556b9e844a9ab
Aaron Lee <aaron@myorange.io>
2025-09-14 18:24:37 +0900

    modify: Update index for room member

--- apps/api/src/entity/room-member.entity.ts
+++ apps/api/src/entity/room-member.entity.ts
@@ -1,9 +1,11 @@
-import { Entity, PrimaryColumn, Column } from 'typeorm';
+import { Entity, PrimaryColumn, Column, Index } from 'typeorm';
 import { BaseTimeEntity } from './base-time.entity';
 import { Room } from './room.entity';
 import { User } from './user.entity';
 
 @Entity({ name: 'room_member' })
+@Index('idx_primary', ['roomId', 'userId'], { unique: true })
+@Index('idx_room_id_created_at', ['roomId', 'createdAt'])
 export class RoomMember extends BaseTimeEntity {
   @PrimaryColumn('uuid')
   roomId: string;
